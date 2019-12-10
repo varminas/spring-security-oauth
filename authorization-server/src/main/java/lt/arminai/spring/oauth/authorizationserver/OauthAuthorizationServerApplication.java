871f23b6c38ae143e44e1fe80c37aa7510edf64d
vytautas.arminas <vytautas.arminas@gmai.com>
2019-12-10 21:00:30 +0100

    Add resource and authorization servers base

---
+++ authorization-server/src/main/java/lt/arminai/spring/oauth/authorizationserver/OauthAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package lt.arminai.spring.oauth.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ authorization-server/src/test/java/lt/arminai/spring/oauth/authorizationserver/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.arminai.spring.oauth.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
